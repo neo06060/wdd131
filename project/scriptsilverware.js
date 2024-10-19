73a9b84b412799cbc08193cc1fd394b1e054ee68
neo06060 <saulcgaos@gmail.com>
2024-10-19 22:59:22 +0200

    a

--- project/scriptsilverware.js
+++ project/scriptsilverware.js
@@ -4,13 +4,10 @@ function toggleText(id) {
     if (textBox.style.display === "block") {
         textBox.style.display = "none";
     } else {
-        // Hide other open text boxes before showing the current one
         var allTextBoxes = document.querySelectorAll('.text-box');
         allTextBoxes.forEach(function(box) {
             box.style.display = 'none';
         });
-
-        // Display the clicked one
         textBox.style.display = "block";
     }
 }
