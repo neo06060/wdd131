8c9d43fdbbdd06d438a424d0ec16fad7a5faffdf
neo06060 <saulcgaos@gmail.com>
2024-10-24 20:45:25 +0200

    a

--- project/scriptsilverware.js
+++ project/scriptsilverware.js
@@ -1,14 +1,19 @@
-function toggleText(id) {
-    var textBox = document.getElementById(id);
 
-    if (textBox.style.display === "block") {
-        textBox.style.display = "none";
+document.getElementById("menuButton").addEventListener("click", function() {
+    var sidePanel = document.getElementById("sidePanel");
+    var body = document.body;
+    
+    if (sidePanel.classList.contains("open")) {
+        sidePanel.classList.remove("open");
+        body.classList.remove("panel-open");
     } else {
-        var allTextBoxes = document.querySelectorAll('.text-box');
-        allTextBoxes.forEach(function(box) {
-            box.style.display = 'none';
-        });
-        textBox.style.display = "block";
+        sidePanel.classList.add("open");
+        body.classList.add("panel-open");
     }
+});
+function toggleText(id) {
+    const textBox = document.getElementById(id);
+    textBox.style.display = textBox.style.display === 'block' ? 'none' : 'block';
 }
+
 
